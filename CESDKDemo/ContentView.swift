91b80bd621e2d4c79f5dfb3a7151d64b4fdb05ec
Matthew Jonat <matthew.jonat@gmail.com>
2025-06-13 16:06:49 +0200

    add photo picker

--- CESDKDemo/ContentView.swift
+++ CESDKDemo/ContentView.swift
@@ -1,13 +1,27 @@
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    @State private var photoItem: PhotosPickerItem?
+    
     var body: some View {
         NavigationView {
-            VStack {
+            VStack(spacing: 20) {
                 Text("📔 My Journal")
                     .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                PhotosPicker("Add New Memory", selection: $photoItem, matching: .images)
+                    .font(.title2)
+                    .foregroundColor(.blue)
+                    .onChange(of: photoItem) { newValue in
+                        // TODO: Handle image selection
+                        print("Image selected: \(newValue)")
+                    }
+                
                 Spacer()
             }
+            .padding()
         }
     }
 }
