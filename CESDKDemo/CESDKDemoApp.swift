247d5c2e07710bf9e1aca091b3b8b7adbcc3316b
Matthew Jonat <matthew.jonat@gmail.com>
2025-06-13 16:02:03 +0200

    inital xcode boilerplate

---
+++ CESDKDemo/CESDKDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CESDKDemoApp.swift
+//  CESDKDemo
+//
+//  Created by Matthew Jonat on 13.06.25.
+//
+
+import SwiftUI
+
+@main
+struct CESDKDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CESDKDemoTests/CESDKDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  CESDKDemoTests.swift
+//  CESDKDemoTests
+//
+//  Created by Matthew Jonat on 13.06.25.
+//
+
+import Testing
+@testable import CESDKDemo
+
+struct CESDKDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
